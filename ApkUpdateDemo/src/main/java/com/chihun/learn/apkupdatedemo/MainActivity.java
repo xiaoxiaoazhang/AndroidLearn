3c240d10e5ef576c4bb5b8ec3f8f2012329cc982
chihun <2878685550@qq.com>
2017-10-17 19:34:04 +0800

    1、downloadmanager   适应7.0, 增加write sdcard权限

--- ApkUpdateDemo/src/main/java/com/chihun/learn/apkupdatedemo/MainActivity.java
+++ ApkUpdateDemo/src/main/java/com/chihun/learn/apkupdatedemo/MainActivity.java
@@ -1,15 +1,24 @@
 package com.chihun.learn.apkupdatedemo;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.chihun.learn.apkupdatedemo.services.UpdateService;
 import com.chihun.learn.apkupdatedemo.services.UpdateService2;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,8 +48,40 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View view) {
         if (view.getId() == R.id.button) {
-            startService(new Intent(this, UpdateService2.class));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                } else {
+                    onUpdate();
+                }
+            } else {
+                onUpdate();
+            }
+//            startService(new Intent(this, UpdateService2.class));
 //            shareAppShop(this, "com.tencent.mm");
+        }
+    }
+    private void onUpdate() {
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/mydemo/";
+        Log.i("=====", "dir: " + dirPath);
+        File dir = new File(dirPath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        startService(new Intent(this, UpdateService2.class));
+    }
+
+    /**
+     * 权限的结果回调函数
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            boolean permissionFlag = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+            if (permissionFlag) {
+                onUpdate();
+            }
         }
     }
 }
